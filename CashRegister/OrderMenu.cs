db3d0ee0f3a3d61894c13646aceeb278fb2a6ee0
Ethan Conner <Ethan.Conner@componentone.com>
2018-01-15 16:30:17 -0500

    Added class Order, MenuItem

--- CashRegister/OrderMenu.cs
+++ CashRegister/OrderMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CashRegister
 {
-    public partial class Form1 : Form
+    public partial class OrderMenu : Form
     {
-        public Form1()
+        public OrderMenu()
         {
             InitializeComponent();
         }
